14a717a15f7656eb1d9b876a2c2397a2ac22257e
yudi <122344097+yudiiiiiiiiiiii@users.noreply.github.com>
2023-09-11 09:51:30 +0700

    Add files via upload

---
+++ tipeadataphp.php
@@ -0,0 +1,15 @@
+<?php
+	echo "____Tipe Data Float____ ";
+	echo "<br>";
+	$a = 5.234;
+	$b = 3.2e4;
+	$c = 4e-10;
+	var_dump($a);
+	echo "<br>";
+	var_dump($b);
+	echo "<br>";
+	var_dump($c);
+	echo "<br><br>";
+	
+
+?>
